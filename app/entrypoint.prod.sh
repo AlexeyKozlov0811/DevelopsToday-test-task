081ec07e0370111b0f54e3b43f379fd64cdcda01
AlexeyKozlov0811 <alexeykozlov0811@gmail.com>
2021-07-29 20:42:56 +0300

    heroku deploying fixes

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -11,4 +11,8 @@ then
     echo "PostgreSQL started"
 fi
 
+CMD python manage.py makemigrations --no-input
+CMD python manage.py migrate
+CMD gunicorn app.wsgi:application --bind 0.0.0.0:$PORT
+
 exec "$@"
